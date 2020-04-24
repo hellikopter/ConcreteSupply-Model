80ca510bec509888ce92314b3094ac761f2cf419
Wojciech Stawiarski <hellikopter@users.noreply.github.com>
2020-04-24 16:36:38 +0100

    correct names

--- ConcreteSupply.Model/ContactPoint.cs
+++ ConcreteSupply.Model/ContactPoint.cs
@@ -3,7 +3,7 @@
     public class ContactPoint: BaseResource
     {
         public int Id { get; set; }
-        public string Label { get; set; }
+        public string ContactPointLabel { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string MobilePhoneNumber { get; set; }
--- ConcreteSupply.Model/Customer.cs
+++ ConcreteSupply.Model/Customer.cs
@@ -3,6 +3,6 @@
     public class Customer : BaseResource
     {
         public int Id { get; set; }
-        public string Label { get; set; }
+        public string ContactPointLabel { get; set; }
     }
 }
